c10925ae0b5d1468cd2bd9e5cf6e739168c7be66
visavi <visavi.net@mail.ru>
2023-06-12 16:55:57 +0300

    Обновил зависимости

--- examples/index.php
+++ examples/index.php
@@ -25,7 +25,7 @@ $api = new Rzd\Api($config);
 </pre>
 
 <h3>Выбор маршрута в одну сторону</h3>
-<a href="/examples/train_routes.php">Просмотр</a><br>
+<a href="train_routes.php">Просмотр</a><br>
 В примере выполняется поиск маршрута САНКТ-ПЕТЕРБУРГ - МОСКВА (только с билетами) на завтра
 
 <pre style="background: aliceblue; padding: 5px; border: 1px solid brown">
@@ -42,7 +42,7 @@ $routes = $api->trainRoutes($params)
 </pre>
 
 <h3>Выбор маршрута в одну сторону c измененными параметрами</h3>
-<a href="/examples/train_routes_params.php">Просмотр</a><br>
+<a href="train_routes_params.php">Просмотр</a><br>
 В примере выполняется аналогичный поиск маршрута SANKT-PETERBURG - MOSKVA (только с билетами) на завтра, но с установленными параметрами:<br>
 <pre style="background: aliceblue; padding: 5px; border: 1px solid brown">
 Язык: en
@@ -51,7 +51,7 @@ Referer: rzd.ru
 </pre>
 
 <h3>Выбор маршрута туда-обратно</h3>
-<a href="/examples/train_routes_return.php">Просмотр</a><br>
+<a href="train_routes_return.php">Просмотр</a><br>
 В примере выполняется поиск маршрута САНКТ-ПЕТЕРБУРГ - МОСКВА (только с билетами) на завтра туда и через 5 дней обратно
 
 <pre style="background: aliceblue; padding: 5px; border: 1px solid brown">
@@ -69,7 +69,7 @@ $routes = $api->trainRoutesReturn($params);
 </pre>
 
 <h3>Выбор маршрута в одну сторону с пересадками</h3>
-<a href="/examples/train_routes_transfer.php">Просмотр</a><br>
+<a href="train_routes_transfer.php">Просмотр</a><br>
 В примере выполняется поиск маршрута НОВЫЙ УРЕНГОЙ - АБАКАН (только с билетами) (с пересадками) на завтра
 
 <pre style="background: aliceblue; padding: 5px; border: 1px solid brown">
@@ -87,7 +87,7 @@ $routes = $api->trainRoutes($params)
 </pre>
 
 <h3>Выбор вагонов</h3>
-<a href="/examples/train_carriages.php">Просмотр</a><br>
+<a href="train_carriages.php">Просмотр</a><br>
 В примере выполняется просмотр всех вагонов в поезде с направлением САНКТ-ПЕТЕРБУРГ - МОСКВА на завтра
 
 <pre style="background: aliceblue; padding: 5px; border: 1px solid brown">
@@ -104,7 +104,7 @@ $carriages = $api->trainCarriages($params)
 </pre>
 
 <h3>Просмотр станций</h3>
-<a href="/examples/train_station_list.php">Просмотр</a><br>
+<a href="train_station_list.php">Просмотр</a><br>
 В примере выполняется поиск всех станций остановок для поезда номер 072E на завтра
 
 <pre style="background: aliceblue; padding: 5px; border: 1px solid brown">
@@ -118,7 +118,7 @@ $stations = $api->trainStationList($params);
 
 
 <h3>Просмотр списка кодов станций</h3>
-<a href="/examples/station_code.php">Просмотр</a><br>
+<a href="station_code.php">Просмотр</a><br>
 В примере выполняется поиск кодов станций начинающихся с ЧЕБ
 
 <pre style="background: aliceblue; padding: 5px; border: 1px solid brown">
